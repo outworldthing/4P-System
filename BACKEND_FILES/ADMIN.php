a9e705d3e1aa57c9c8d62ed1f65c526725043c05
Jeffrey Ajero <thegelouy@gmail.com>
2020-06-11 18:03:53 +0800

    added everything from rommel's email

---
+++ BACKEND_FILES/ADMIN.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class ADMIN {
+
+    public $servername = "";
+    public $username = "";
+    public $password = "";
+    public $database = "";
+    public $conn = "";
+
+    function __construct() {
+        $this->servername = "localhost";
+        $this->username = "root";
+        $this->password = "rootPass123";
+        $this->database = "FourP's_SQL";
+
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
+        if (!$this->conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+    }
+    
+    function ConfirmAdminUsernamePassword($username, $password) {
+        try {
+            $command = "Select * from AdminAccount where AdminUsername ='" . $username . "' AND AdminPassword='" . $password . "'";
+            $result = mysqli_query($this->conn, $command);
+            if (mysqli_num_rows($result)) {
+                return TRUE;
+            }
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+        return FALSE;
+    }
+    
+    function checkUsernameExist($username){
+        try {
+            $command = "Select AdminUsername from AdminAccount where AdminUsername ='" . $username . "'";
+            $result = mysqli_query($this->conn, $command);
+            if (mysqli_num_rows($result)>0) {
+                return TRUE;
+            }
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+        return FALSE;
+    }
+    
+    function createClientAccount($username, $password,$fname,$mname,$lname,$suffix) {
+        try {
+            $command = "Insert into AdminAccount(AdminUsername,AdminPassword,AdminFName,AdminMName"
+                    . "AdminLName,AdminSuffix) "
+                    . "VALUES($username,$password,$fname,$mname,$lname,$suffix)";
+            $result = mysqli_query($this->conn, $command);
+            if (mysqli_num_rows($result)) {
+                return TRUE;
+            }
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+        return FALSE;
+    }
+
+}
---
+++ SQL/FourP's_SQL.sql
@@ -0,0 +1,238 @@
+Create Table FourP;
+use FourP;
+
+Create table Family(
+FamilyID int not null auto_increment,
+DateOfRegistry date  not null,
+Region varchar(50) not null,
+Municipality varchar(50) not null,
+Province varchar(50) not null,
+Barangay varchar(50) not null,
+Street varchar(50) not null,
+HouseNo varchar(50),
+Subdivision varchar(50),
+HealthGrant int default 750,
+primary key (FamilyID)
+);
+
+Create table FamilyAccount(
+FamilyAccountID int not null auto_increment,
+FamilyID int not null,	
+HealthBank int not null ,
+HealthBenefitReceivedDate date,
+Primary key (FamilyAccountID),
+foreign key (FamilyID) references Family(FamilyID)
+);
+
+Create Table Members(
+MemberID int not null auto_increment,
+FamilyID int not null,
+FirstName varchar(50) not null,
+MiddleName varchar(50),
+LastName varchar(50) not null,
+Suffix varchar(10),
+BirthDate date not null,
+Gender tinyint(1) not null,
+DateOfRegistry date,
+Primary Key (MemberID),
+foreign key (FamilyID) references Family(FamilyID)on delete cascade on update cascade
+);
+
+Create Table EducationAccount(
+EducationID int not null auto_increment ,
+MemberID int not null,
+EducationBank int not null,
+EducatonBenefitReceivedDate date,
+Primary Key (EducationIDs)
+);
+
+Create Table School(
+SchoolID int not null auto_increment,
+SchoolName varchar(150) not null,
+Primary Key(SchoolID)
+);
+
+Create Table Hospital(
+HospitalID int not null auto_increment,
+HospitalName varchar(50) not null,
+Primary Key (HospitalID)
+);
+
+Create Table DayCareElem(
+DayCareElemID int not null auto_increment,
+Subsidy int not null default 300,
+MemberID int not null,
+SchoolID int not null,
+Primary Key (DayCareElemID),
+foreign key (MemberID) references Members(MemberID)on delete cascade on update cascade,
+foreign key (SchoolID) references School(SchoolID)on delete cascade on update cascade
+);
+
+Create Table SHS(
+SHSID int not null auto_increment,
+Subsidy int not null default 700,
+MemberID int not null,
+SchoolID int not null,
+Primary Key (SHSID),
+foreign key (MemberID) references Members(MemberID)on delete cascade on update cascade,
+foreign key (SchoolID) references School(SchoolID)on delete cascade on update cascade
+);
+
+Create Table JHS(
+JHSID int not null auto_increment,
+Subsidy int not null default 700,
+MemberID int not null,
+SchoolID int not null,
+Primary Key (JHSID),
+foreign key (MemberID) references Members(MemberID)on delete cascade on update cascade,
+foreign key (SchoolID) references School(SchoolID)on delete cascade on update cascade
+);
+
+Create table Hospital(
+HospitalID int not null auto_increment,
+HospitalName varchar(50) not null,
+Primary key (HospitalID)
+);
+
+Create table Prenatal(
+PrenatalID int not null auto_increment,
+PrenatalDate date not null,
+HospitalID int not null,
+DoctorFName varchar(50) not null,
+DoctorMName varchar(50) ,
+DoctorLName varchar(50) not null,	
+DoctorSuffix varchar(50),
+primary key (PrenatalID),
+foreign key (HospitalID) references Hospital(HospitalID)on delete cascade on update cascade,
+foreign key (MemberID) references Members(MemberID)on delete cascade on update cascade
+);
+
+Create table Postnatal(
+PostnatalID int not null auto_increment,
+PostnatalDate date not null,
+HospitalID int not null,
+MemberID int not null,
+DoctorFName varchar(50) not null,
+DoctorMName varchar(50) ,
+DoctorLName varchar(50) not null,
+DoctorSuffix varchar(50),
+primary key (PostnatalID),
+foreign key (HospitalID) references Hospital(HospitalID)on delete cascade on update cascade,
+foreign key (MemberID) references Members(MemberID)on delete cascade on update cascade
+);
+
+Create Table Pregnant(
+PregnantID int not null auto_increment,
+MemberID int not null,
+PrenatalID int,
+PostnatalID int,
+primary key (PregnantID),
+foreign key(MemberID) references Members(MemberID)on delete cascade on update cascade,
+foreign key (PrenatalID) references Prenatal(PrenatalID)on delete cascade on update cascade,
+foreign key (PostnatalID) references Postnatal(PostnatalID)on delete cascade on update cascade
+);
+
+Create table MemberAccount(
+MemberAccountID int not null auto_increment,
+MemberID int not null,
+MemberUsername varchar(50) not null,
+MemberPassword varchar(50) not null,
+Primary Key (MemberAccountID),
+foreign key(MemberID) references Members(MemberID)on delete cascade on update cascade
+);
+
+Create table AdminAccount(
+AdminID int not null auto_increment,
+AdminUsername varchar(50) not null,
+AdminPassword varchar(50) not null,
+AdminFName varchar(50) not null,
+AdminMName varchar(50) ,
+AdminLName varchar(50) not null,
+AdminSuffix varchar(50),
+Primary key (AdminID)
+);
+
+Create Table FamilySession(
+FamilySessionID int not null auto_increment,
+FamilyID int not null,
+Venue varchar(50) not null,
+DateOfSession date,
+Primary Key (FamilySessionID),
+Foreign Key (FamilyID) references Family(FamilyID)on delete cascade on update cascade
+);
+
+Create Table CheckupAndVaccine(
+CheckupAndVaccineID int not null auto_increment,
+MemberID int not null,
+Venue varchar(50) not null,
+DoctorFName varchar(50) not null,
+DoctorMName varchar(50) ,
+DoctorLName varchar(50) not null,
+DoctorSuffix varchar(50),
+VaccineType varchar(50),
+CheckUpDiagnosis varchar(200) not null,
+DateOfSession date,
+Primary Key (CheckupAndVaccineID),
+foreign key (MemberID) references Members(MemberID)on delete cascade on update cascade
+);
+
+Create Table Deworming(
+DewormingID int not null auto_increment,
+MemberID int not null,
+Venue varchar(50) not null,
+DoctorFName varchar(50) not null,
+DoctorMName varchar(50) ,
+DoctorLName varchar(50) not null,
+DoctorSuffix varchar(50),
+DewormingGenericName varchar(50) not null,
+DateOfSession date,
+Primary Key (CheckupAndVaccineID),
+foreign key (MemberID) references Members(MemberID)on delete cascade on update cascade
+);
+
+Create Table Teacher(
+TeacherID int not null auto_increment,
+TeacherFname varchar(50) not null,
+TeacherMname varchar(50) ,
+TeacherLname varchar(50) not null,
+TeacherSuffix varchar(50),
+TeacherBirthDate date not null,
+TeacherUsername varchar(50),
+TecherPassword varchar(50),
+Primary Key (TeacherID)
+);
+
+Create Table Student(
+StudentID int not null auto_increment,
+MemberID int not null,
+Primary Key (StudentID),
+foreign key (MemberID) references Members(MemberID) on delete cascade on update cascade,
+);
+
+Create Table Section(
+SectionID int not null auto_increment,
+SectionName varchar(50) not null,
+StudentID int not null,
+TeacherID int not null,
+Primary key (SectionID),
+foreign key (StudentID) references Student(StudentID) on delete cascade on update cascade,
+foreign key (TeacherID) references Teacher(TeacherID) on delete cascade on update cascade
+);
+
+Create Table Attendance(
+AttendanceID int not null auto_increment,
+StudentID int not null,
+TotalNumberOfSchoolDays int not null,
+TotalNumberOfAbsents int not null,
+Primary key (AttendanceID),
+foreign key (StudentID) references Student(StudentID)on delete cascade on update cascade
+);
+
+Create table UpdateLog(
+UpdateLogID int not null auto_increment,
+MemberID int not null,
+DateOfPublish date not null,
+UpdateLogDetails varchar(150) not null,
+Primary Key (UpdateLogID),
+foreign key (MemberID) references Members(MemberID)on delete cascade on update cascade
+);
