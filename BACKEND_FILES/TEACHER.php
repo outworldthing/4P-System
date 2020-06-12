ea8f93f7b1624c97a3c624c71ea0d1b3eda25345
Rommel Urbano Jr <50763887+RommelUrbano07@users.noreply.github.com>
2020-06-12 12:43:28 +0800

    Update TEACHER.php

--- BACKEND_FILES/TEACHER.php
+++ BACKEND_FILES/TEACHER.php
@@ -1,1 +1,37 @@
 <?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class TEACHER {
+
+    public $servername = "";
+    public $username = "";
+    public $password = "";
+    public $database = "";
+    public $conn = "";
+
+    function __construct() {
+        $this->servername = "localhost";
+        $this->username = "root";
+        $this->password = "rootPass123";
+        $this->database = "FourP's_SQL";
+
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
+        if (!$this->conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+    }
+    
+    function confirmTeacherAccount(){
+        
+    }
+    
+    function SignUpTeacherAccount(){
+        
+    }
+
+}
